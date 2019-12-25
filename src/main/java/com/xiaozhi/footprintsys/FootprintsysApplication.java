136db17d4e133699dcfba90e2b3e5cc704676853
stephen.wzy <stephen.wzy@alibaba-inc.com>
2019-12-25 16:47:05 +0800

    intinal project

---
+++ src/main/java/com/xiaozhi/footprintsys/FootprintsysApplication.java
@@ -0,0 +1,13 @@
+package com.xiaozhi.footprintsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootprintsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootprintsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaozhi/footprintsys/FootprintsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaozhi.footprintsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootprintsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
